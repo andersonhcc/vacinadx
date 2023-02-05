854b4344208fc88abe2803a1515e7523e7378812
andersonhcc <programadorandersonh@gmail.com>
2023-02-05 10:23:56 -0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {ThemeProvider} from 'styled-components/native';
 import {Home} from './screens/Home';
-
 import useAppearance from './hooks/useAppearance';
 
 const App: React.FC = () => {
