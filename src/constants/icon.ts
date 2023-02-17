adedb074ca13e3f95b0e211f6d5022887933869d
andersonhcc <programadorandersonh@gmail.com>
2023-02-17 08:12:28 -0300

    screen login

--- src/constants/icon.ts
+++ src/constants/icon.ts
@@ -2,13 +2,20 @@ import dog from '../assets/images/dog.png';
 import eyeOpen from '../assets/images/view.png';
 import eyeClose from '../assets/images/hidden.png';
 
+import vaccinePlus from '../assets/icons/vaccine-plus/Vector.png';
 import doctor from '../assets/icons/doctor/Vector.png';
 
-export type NameIconType = 'dog' | 'eyeOpen' | 'eyeClose' | 'doctor';
+export type NameIconType =
+  | 'dog'
+  | 'eyeOpen'
+  | 'eyeClose'
+  | 'doctor'
+  | 'vaccinePlus';
 
 export default {
   dog,
   eyeOpen,
   eyeClose,
   doctor,
+  vaccinePlus,
 };
--- src/screens/auth/Acess/indes.tsx
+++ src/screens/auth/Acess/indes.tsx
@@ -1,10 +1,68 @@
 import React from 'react';
-// import {View} from 'react-native';
+import {StatusBar} from 'react-native';
 
-import {Container} from './styles';
+import {
+  Container,
+  ContainerRounded,
+  Column,
+  IconRoundedVaccine,
+  IconVaccine,
+} from './styles';
+
+import splashImage from '~/assets/images/splash/splashScreen.png';
+import {Text} from '~/components/Text';
+import {Button} from '~/components/Button';
+import {Separator} from '~/components/Separator';
+import {useTheme} from 'styled-components';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import useSignInNavigation from '~/hooks/useSignInNavigator';
 
 const Acess: React.FC = () => {
-  return <Container />;
+  const {bottom} = useSafeAreaInsets();
+  const {spacing} = useTheme();
+  const navigation = useSignInNavigation();
+
+  /**
+   *
+   */
+
+  const handleNavigationToLogin = () => {
+    navigation.navigate('login');
+  };
+
+  const handleNavigationToSignUp = () => {
+    navigation.navigate('signUp');
+  };
+
+  return (
+    <Container source={splashImage}>
+      <StatusBar barStyle="light-content" />
+
+      <Column>
+        <IconRoundedVaccine>
+          <IconVaccine icon="vaccinePlus" size={80} />
+        </IconRoundedVaccine>
+        <Separator height={spacing.md} />
+        <Text typography="h2" color="background">
+          Vacinadx
+        </Text>
+      </Column>
+
+      <ContainerRounded>
+        <Separator height={spacing.xl} />
+        <Text typography="h4">Bem vindx</Text>
+        <Separator height={spacing.sm} />
+        <Text typography="subtitle1">Acesse ao app</Text>
+        <Separator height={spacing.xxl} />
+        <Button onPress={handleNavigationToLogin}>Login</Button>
+        <Separator height={spacing.lg} />
+        <Button mode="outlined" onPress={handleNavigationToSignUp}>
+          Cadastro
+        </Button>
+        <Separator height={bottom + spacing.xxxl} />
+      </ContainerRounded>
+    </Container>
+  );
 };
 
 export {Acess};
