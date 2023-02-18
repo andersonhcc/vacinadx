2e15430adcdc5476e16d6a5302ca6f832755b72a
andersonhcc <programadorandersonh@gmail.com>
2023-02-18 14:58:33 -0300

    test

--- src/screens/auth/SignUp/index.tsx
+++ src/screens/auth/SignUp/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import {Text} from '~/components/Text';
 
 import {Container} from './styles';
 
 const SignUp: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Text>Testando</Text>
+    </Container>
+  );
 };
 
 export {SignUp};
